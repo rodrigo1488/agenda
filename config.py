18a3facd35b2cdbac16de1b7c0de5602b048774a
rodrigo gomes <gomesrodrigo528@gmail.com>
2025-01-29 23:04:08 -0300

    att

--- config.py
+++ config.py
@@ -52,6 +52,7 @@ def atualizar_configuracao():
         "endereco": request.form.get("endereco"),
         "cep": request.form.get("cep"),
         "tel_empresa": request.form.get("tel_empresa"),
+        
         "status": request.form.get("status"),
         "descricao": request.form.get("descricao"),
         "cor_emp": request.form.get("cor")
