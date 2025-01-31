68249de0ca4d1f9542804910a0452beed2488e71
rodrigo gomes <gomesrodrigo528@gmail.com>
2025-01-31 20:21:44 -0300

    debug false

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ def inicio():
 # Iniciar o agendador
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
